54f5bfe5c51ceebc6cf16dc34f721415af5e97ea
Jake Herrmann <jtherrmann@alaska.edu>
2022-04-14 11:07:06 -0800

    Deploy Earthdata and PDC creds

--- lambda/src/main.py
+++ lambda/src/main.py
@@ -1,10 +1,9 @@
-import argparse
 import os
 from datetime import datetime, timezone
 
 import requests
 
-# TODO what's the difference?
+# TODO make url configurable
 PDC_URL_TEST = 'https://testsentry.pdc.org'
 PDC_URL_PROD = 'https://sentry.pdc.org'
 
@@ -29,15 +28,15 @@ def get_hyp3_api_session(username, password) -> requests.Session:
     return session
 
 
-def get_existing_subscriptions(session: requests.Session) -> dict:
-    url = f'{HYP3_URL_TEST}/subscriptions'
+def get_existing_subscriptions(session: requests.Session, hyp3_url: str) -> dict:
+    url = f'{hyp3_url}/subscriptions'
     response = session.get(url)
     response.raise_for_status()
     return response.json()
 
 
-def submit_subscription(session: requests.Session, subscription: dict, validate_only=False) -> dict:
-    url = f'{HYP3_URL_TEST}/subscriptions'
+def submit_subscription(session: requests.Session, hyp3_url: str, subscription: dict, validate_only=False) -> dict:
+    url = f'{hyp3_url}/subscriptions'
     subscription['validate_only'] = validate_only
     response = session.post(url, json=subscription)
     response.raise_for_status()
@@ -105,18 +104,11 @@ def get_env_var(name: str) -> str:
     return val
 
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pdc-prod', action='store_true')
-    return parser.parse_args()
-
-
 def lambda_handler(event, context) -> None:
-    args = parse_args()
+    pdc_api_url = PDC_URL_TEST
+    hyp3_url = HYP3_URL_TEST
 
-    pdc_api_url = PDC_URL_PROD if args.pdc_prod else PDC_URL_TEST
-
-    auth_token = get_env_var('PDC_HAZARDS_AUTH_TOKEN_PROD' if args.pdc_prod else 'PDC_HAZARDS_AUTH_TOKEN_TEST')
+    auth_token = get_env_var('PDC_HAZARDS_AUTH_TOKEN')
     earthdata_username = get_env_var('EARTHDATA_USERNAME')
     earthdata_password = get_env_var('EARTHDATA_PASSWORD')
 
@@ -131,4 +123,4 @@ def lambda_handler(event, context) -> None:
     subscriptions = list(map(get_hyp3_subscription, hazards))
     for subscription in subscriptions:
         # TODO remove validate_only
-        submit_subscription(session, subscription, validate_only=True)
+        submit_subscription(session, hyp3_url, subscription, validate_only=True)
