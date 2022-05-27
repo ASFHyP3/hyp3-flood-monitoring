ade61182ab35c54b036802574233e326789e76dc
Jake Herrmann <jtherrmann@alaska.edu>
2022-05-27 13:54:14 -0800

    Add hyp3 API class

--- lambda/src/hyp3_floods.py
+++ lambda/src/hyp3_floods.py
@@ -23,6 +23,41 @@ class OutdatedAOI(Exception):
     pass
 
 
+class HyP3SubscriptionsAPI:
+
+    def __init__(self, api_url: str, username: str, password: str):
+        self._url = api_url
+        self._session = self._get_hyp3_api_session(username, password)
+
+    @staticmethod
+    def _get_hyp3_api_session(username: str, password: str) -> requests.Session:
+        url = 'https://urs.earthdata.nasa.gov/oauth/authorize?response_type=code&client_id=BO_n7nTIlMljdvU6kRRB3g' \
+              '&redirect_uri=https://auth.asf.alaska.edu/login&app_type=401'
+        session = requests.Session()
+        response = session.get(url, auth=(username, password))
+        response.raise_for_status()
+        return session
+
+    def get_subscriptions_by_name(self, name: str) -> dict:
+        url = f'{self._url}/subscriptions'
+        response = self._session.get(url, params={'name': name})
+        response.raise_for_status()
+        return response.json()
+
+    def submit_subscription(self, subscription: dict, validate_only=False) -> dict:
+        url = f'{self._url}/subscriptions'
+        payload = {'subscription': subscription, 'validate_only': validate_only}
+        response = self._session.post(url, json=payload)
+        response.raise_for_status()
+        return response.json()
+
+    def update_subscription(self, subscription_id: str, end: str) -> dict:
+        url = f'{self._url}/subscriptions/{subscription_id}'
+        response = self._session.patch(url, json={'enabled': True, 'end': end})
+        response.raise_for_status()
+        return response.json()
+
+
 def get_active_hazards(auth_token: str) -> list[dict]:
     url = f'{PDC_URL}/hp_srv/services/hazards/t/json/get_active_hazards'
     response = requests.get(url, headers={'Authorization': f'Bearer {auth_token}'})
@@ -30,52 +65,21 @@ def get_active_hazards(auth_token: str) -> list[dict]:
     return response.json()
 
 
-def get_hyp3_api_session(username, password) -> requests.Session:
-    url = 'https://urs.earthdata.nasa.gov/oauth/authorize?response_type=code&client_id=BO_n7nTIlMljdvU6kRRB3g' \
-          '&redirect_uri=https://auth.asf.alaska.edu/login&app_type=401'
-    session = requests.Session()
-    response = session.get(url, auth=(username, password))
-    response.raise_for_status()
-    return session
-
-
-def submit_subscription(session: requests.Session, hyp3_url: str, subscription: dict, validate_only=False) -> dict:
-    url = f'{hyp3_url}/subscriptions'
-    payload = {'subscription': subscription, 'validate_only': validate_only}
-    response = session.post(url, json=payload)
-    response.raise_for_status()
-    return response.json()
-
-
-def get_subscriptions_by_name(session: requests.Session, hyp3_url: str, name: str) -> dict:
-    url = f'{hyp3_url}/subscriptions'
-    response = session.get(url, params={'name': name})
-    response.raise_for_status()
-    return response.json()
-
-
-def get_existing_subscription(session: requests.Session, hyp3_url: str, name: str) -> Optional[dict]:
+def get_existing_subscription(hyp3: HyP3SubscriptionsAPI, name: str) -> Optional[dict]:
     # TODO tests?
-    response = get_subscriptions_by_name(session, hyp3_url, name)
+    response = hyp3.get_subscriptions_by_name(name)
     subscriptions = response['subscriptions']
     if len(subscriptions) > 1:
         raise DuplicateSubscriptionNames(f"Got {len(subscriptions)} with name {name} (expected 0 or 1)")
     return subscriptions[0] if subscriptions else None
 
 
-def update_subscription(session: requests.Session, hyp3_url: str, subscription_id: str, end: str) -> dict:
-    url = f'{hyp3_url}/subscriptions/{subscription_id}'
-    response = session.patch(url, json={'enabled': True, 'end': end})
-    response.raise_for_status()
-    return response.json()
-
-
-def process_active_hazards(session: requests.Session, hyp3_url: str, active_hazards: list[dict], now: datetime) -> None:
+def process_active_hazards(hyp3: HyP3SubscriptionsAPI, active_hazards: list[dict], now: datetime) -> None:
     # TODO tests?
     for count, hazard in enumerate(active_hazards, start=1):
         print(f"({count}/{len(active_hazards)}) Processing hazard {hazard['uuid']}")
         try:
-            process_active_hazard(session, hyp3_url, hazard, now)
+            process_active_hazard(hyp3, hazard, now)
         except (requests.HTTPError, DuplicateSubscriptionNames, OutdatedAOI) as e:
             print(f"Error while processing hazard: {e}")
 
@@ -83,22 +87,22 @@ def process_active_hazards(session: requests.Session, hyp3_url: str, active_haza
 # TODO rename functions to make it clear what's fetching via network (see the get_ functions)
 
 
-def process_active_hazard(session: requests.Session, hyp3_url: str, hazard: dict, now: datetime) -> None:
+def process_active_hazard(hyp3: HyP3SubscriptionsAPI, hazard: dict, now: datetime) -> None:
     # TODO tests?
 
     name = subscription_name_from_hazard_uuid(hazard['uuid'])
     print(f"Fetching existing subscription with name: {name}")
-    existing_subscription = get_existing_subscription(session, hyp3_url, name)
+    existing_subscription = get_existing_subscription(hyp3, name)
 
     if existing_subscription:
         compare_aoi(existing_subscription, get_aoi(hazard))
         subscription_id = existing_subscription['subscription_id']
         print(f"Updating subscription with id: {subscription_id}")
-        update_subscription(session, hyp3_url, subscription_id, get_end_datetime_str(now))
+        hyp3.update_subscription(subscription_id, get_end_datetime_str(now))
     else:
         print('No existing subscription; submitting new subscription')
         new_subscription = get_hyp3_subscription(hazard, now)
-        response = submit_subscription(session, hyp3_url, new_subscription)
+        response = hyp3.submit_subscription(new_subscription)
         subscription_id = response['subscription']['subscription_id']
         print(f"Got subscription id: {subscription_id}")
 
@@ -206,7 +210,7 @@ def lambda_handler(event, context) -> None:
 
     print(f"Earthdata user: {earthdata_username}\n")
 
-    session = get_hyp3_api_session(earthdata_username, earthdata_password)
+    hyp3 = HyP3SubscriptionsAPI(hyp3_url, earthdata_username, earthdata_password)
 
     print('Fetching active hazards')
     active_hazards = get_active_hazards(auth_token)
@@ -216,6 +220,6 @@ def lambda_handler(event, context) -> None:
     print(f"Active hazards (after filtering): {len(active_hazards)}\n")
 
     # TODO refactor to only call get_end_datetime_str here?
-    process_active_hazards(session, hyp3_url, active_hazards, get_now())
+    process_active_hazards(hyp3, active_hazards, get_now())
 
     # TODO finish lambda handler, remove unused functions and tests, update lambda handler tests
