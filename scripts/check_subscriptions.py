72f02a4fec1582b3918996dd22c96e407e6a06d0
Jake Herrmann <jtherrmann@alaska.edu>
2022-06-13 10:08:48 -0800

    Factor out get_query_results()

--- scripts/check_subscriptions.py
+++ scripts/check_subscriptions.py
@@ -13,18 +13,23 @@ def get_subscriptions(session: requests.Session) -> dict:
     return response.json()
 
 
-def get_expected_subscriptions_count() -> int:
-    client = boto3.client('logs')
+def get_query_results(client, **kwargs) -> dict:
+    query_id = client.start_query(**kwargs)['queryId']
 
-    query_id = client.start_query(
+    while (results := client.get_query_results(queryId=query_id))['status'] != 'Complete':
+        time.sleep(0.5)
+
+    return results
+
+
+def get_expected_subscriptions_count(client) -> int:
+    results = get_query_results(
+        client,
         logGroupName='/aws/lambda/hyp3-flood-monitoring-test-Lambda-XUnL4S4ZZ2Cn',
         startTime=0,
         endTime=int(time.time() + 3600),
         queryString='fields @message | filter @message like /Got subscription id/ | stats count()'
-    )['queryId']
-
-    while (results := client.get_query_results(queryId=query_id))['status'] != 'Complete':
-        time.sleep(0.5)
+    )
 
     count = int(results['results'][0][0]['value'])
     assert count == results['statistics']['recordsMatched']
@@ -38,7 +43,8 @@ def main() -> None:
     session = hyp3_floods.HyP3SubscriptionsAPI._get_hyp3_api_session(earthdata_username, earthdata_password)
     subscriptions = get_subscriptions(session)['subscriptions']
 
-    print(f'Expected subscriptions count: {get_expected_subscriptions_count()}')
+    client = boto3.client('logs')
+    print(f'Expected subscriptions count: {get_expected_subscriptions_count(client)}')
     print(f'Actual subscriptions count: {len(subscriptions)}')
 
 
