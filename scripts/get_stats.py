71a22512503f5847004b0762b59573b7f70be40f
Jake Herrmann <jtherrmann@alaska.edu>
2022-06-14 12:08:22 -0800

    Write jobless subscription stats

--- scripts/get_stats.py
+++ scripts/get_stats.py
@@ -1,5 +1,39 @@
+from datetime import datetime
+
 import hyp3_floods
 import _util
+
+
+def get_jobless_subscriptions(subscriptions: list[dict], job_subscription_ids: frozenset[str]) -> list[dict]:
+    return [
+        subscription for subscription in subscriptions
+        if not subscription['enabled']
+        and subscription['subscription_id'] not in job_subscription_ids
+    ]
+
+
+def write_jobless_subscription_stats(jobless_subscriptions: list[dict]) -> None:
+    rows = [('id', 'aoi', 'start', 'end', 'delta')]
+    for jobless_sub in jobless_subscriptions:
+        start = jobless_sub['search_parameters']['start']
+        end = jobless_sub['search_parameters']['end']
+        rows.append((
+            jobless_sub['subscription_id'],
+            jobless_sub['search_parameters']['intersectsWith'],
+            start,
+            end,
+            parse_datetime(end) - parse_datetime(start),
+        ))
+    csv = '\n'.join(','.join(f'"{field}"' for field in row) for row in rows)
+
+    with open('jobless-subscription-stats.csv', 'w') as f:
+        f.write(csv)
+
+    print('Wrote jobless subscription stats')
+
+
+def parse_datetime(datetime_str: str) -> datetime:
+    return datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%SZ')
 
 
 def main() -> None:
@@ -20,6 +54,12 @@ def main() -> None:
     assert job_subscription_ids.issubset(subscription_ids)
 
     print(f'{len(job_subscription_ids)} / {len(subscriptions)} subscriptions have at least one job')
+
+    jobless_subscriptions = get_jobless_subscriptions(subscriptions, job_subscription_ids)
+
+    print(f'{len(jobless_subscriptions)} / {len(subscriptions)} subscriptions are disabled and jobless')
+
+    write_jobless_subscription_stats(jobless_subscriptions)
 
 
 if __name__ == '__main__':
