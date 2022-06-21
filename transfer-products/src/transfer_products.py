096cf9a4d78e6755aad7a549865aff1118dd08d5
Jake Herrmann <jtherrmann@alaska.edu>
2022-06-21 15:04:01 -0800

    Add cli to transfer-products lambda

--- transfer-products/src/transfer_products.py
+++ transfer-products/src/transfer_products.py
@@ -1,2 +1,22 @@
+import argparse
+
+from dotenv import load_dotenv
+
+
 def lambda_handler(event, context) -> None:
-    pass
+    main(dry_run=False)
+
+
+def main(dry_run: bool) -> None:
+    if dry_run:
+        print('(DRY RUN)')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dotenv_path')
+    parser.add_argument('--no-dry-run', action='store_true')
+    args = parser.parse_args()
+
+    load_dotenv(dotenv_path=args.dotenv_path)
+    main(dry_run=(not args.no_dry_run))
