eae78c4d3f799de91cc73169989eebb32f8e22ed
Jake Herrmann <jtherrmann@alaska.edu>
2023-03-08 10:45:14 -0900

    Update check_subscriptions script

--- scripts/check_subscriptions.py
+++ scripts/check_subscriptions.py
@@ -20,20 +20,42 @@ def main() -> None:
     earthdata_username = hyp3_floods.get_env_var('EARTHDATA_USERNAME')
     earthdata_password = hyp3_floods.get_env_var('EARTHDATA_PASSWORD')
 
+    print(f'HyP3 API URL: {hyp3_url}')
+    print(f'Earthdata user: {earthdata_username}\n')
+
+    if 'hyp3-test-api' in hyp3_url:
+        log_group = '/aws/lambda/hyp3-flood-monitoring-test-Lambda-XUnL4S4ZZ2Cn'
+    elif 'hyp3-watermap' in hyp3_url:
+        log_group = '/aws/lambda/hyp3-flood-monitoring-Lambda-q7JXd48mgEhC'
+    else:
+        raise ValueError('HyP3 URL not recognized')
+
     session = hyp3_floods.HyP3SubscriptionsAPI._get_hyp3_api_session(earthdata_username, earthdata_password)
     subscriptions = _util.get_subscriptions(session, hyp3_url)['subscriptions']
 
-    print(f'Total subscriptions (from logs):     {_logs.get_expected_subscriptions_count()}')
-    print(f'Total subscriptions (from HyP3 API): {len(subscriptions)}\n')
-
-    active_hazards_timestamp, active_hazards_count = _logs.get_active_hazards_count()
+    active_hazards_timestamp, active_hazards_count = _logs.get_active_hazards_count(log_group)
     updated_subscriptions_end, updated_subscriptions_count = count_updated_subscriptions(subscriptions)
 
-    print(f'Active hazards (from logs):               {active_hazards_count}')
-    print(f'Up-to-date subscriptions (from HyP3 API): {updated_subscriptions_count}\n')
+    print(
+        'Up-to-date subscriptions are those subscriptions with the\n'
+        'most recent end datetime among the list of HyP3\n'
+        f'subscriptions for the {earthdata_username} user.\n\n'
 
-    print(f'Active hazards log timestamp:          {active_hazards_timestamp}')
-    print(f'Up-to-date subscriptions end datetime: {updated_subscriptions_end}')
+        'The number of up-to-date subscriptions should equal the\n'
+        'number of active hazards:\n'
+    )
+
+    print(f'    Active hazards (from logs):               {active_hazards_count}')
+    print(f'    Up-to-date subscriptions (from HyP3 API): {updated_subscriptions_count}\n')
+
+    print(
+        'Furthermore, all up-to-date subscriptions should have an end\n'
+        f'datetime approximately {hyp3_floods.HAZARD_END_DATE_DELTA} ahead of the time of the most\n'
+        'recent flood monitoring Lambda execution:\n'
+    )
+
+    print(f'    Most recent Lambda execution, approximate (from logs): {active_hazards_timestamp}')
+    print(f'    Up-to-date subscription end datetime (from HyP3 API):  {updated_subscriptions_end}\n')
 
 
 if __name__ == '__main__':
